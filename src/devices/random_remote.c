34311f76637a007b07f213adf77a22d45334ec56
Low Heok Hong <heok.hong.low@gmail.com>
2024-07-09 02:55:50 +0800

    More controls

--- src/devices/random_remote.c
+++ src/devices/random_remote.c
@@ -1,37 +1,34 @@
 #include "decoder.h"
 
 static int brandless_remote_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
-	if (bitbuffer->bits_per_row[0] < 24) {
+	if (bitbuffer->bits_per_row[0] < 24 || bitbuffer->bits_per_row[0] > 30) {
 		return DECODE_ABORT_LENGTH;
 	}
   uint8_t data[3]={0};
 	bitbuffer_extract_bytes(bitbuffer, 0, 0, data, 24);
-  if (data[0] != 0x65 && data[1] != 0x59) {
-    return DECODE_ABORT_EARLY;
-  }
 	char hexstr[7] = {0};
 
 	const char HEX [16] = "0123456789ABCDEF";
 	for(size_t i=0; i<3; i++) {
-    	hexstr[i*2] =  HEX[ (data[i] & 0xF0) >> 4 ];
-    	hexstr[i*2+1] =  HEX[ (data[i] & 0x0F) ];
-    }
+    hexstr[i*2] =  HEX[ (data[i] & 0xF0) >> 4 ];
+    hexstr[i*2+1] =  HEX[ (data[i] & 0x0F) ];
+  }
 
-    /* clang-format off */
-    data_t *output = data_make(
-            "model",        "",             DATA_STRING, "Brandless remote",
-            "id",           "Id",           DATA_STRING, hexstr,
-            NULL);
-    /* clang-format on */
+  /* clang-format off */
+  data_t *output = data_make(
+          "model",        "",             DATA_STRING, "Brandless remote",
+          "id",           "Id",           DATA_STRING, hexstr,
+          NULL);
+  /* clang-format on */
 
-    decoder_output_data(decoder, output);
-    return 1;
+  decoder_output_data(decoder, output);
+  return 1;
 }
 
 static char const *const brandless_remote_output_fields[] = {
 		"model",
-        "id",
-        NULL,
+    "id",
+    NULL,
 };
 
 r_device const brandless_remote = {
